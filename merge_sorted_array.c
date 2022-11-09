d0a92447fc70180ac7937cf71d245d1552daa8c5
Popuri Keerthika <79918843+keerthikapopuri@users.noreply.github.com>
2022-11-09 16:06:26 +0530

    Update merge_sorted_array.c

--- merge_sorted_array.c
+++ merge_sorted_array.c
@@ -1,20 +1,58 @@
+88. Merge Sorted Array
+You are given two integer arrays nums1 and nums2, sorted in non-decreasing order, and two integers m and n, representing the number of elements in nums1 and nums2 respectively.
+
+Merge nums1 and nums2 into a single array sorted in non-decreasing order.
+
+The final sorted array should not be returned by the function, but instead be stored inside the array nums1. To accommodate this, nums1 has a length of m + n, where the first m elements denote the elements that should be merged, and the last n elements are set to 0 and should be ignored. nums2 has a length of n.
+
+ 
+
+Example 1:
+
+Input: nums1 = [1,2,3,0,0,0], m = 3, nums2 = [2,5,6], n = 3
+Output: [1,2,2,3,5,6]
+Explanation: The arrays we are merging are [1,2,3] and [2,5,6].
+The result of the merge is [1,2,2,3,5,6] with the underlined elements coming from nums1.
+Example 2:
+
+Input: nums1 = [1], m = 1, nums2 = [], n = 0
+Output: [1]
+Explanation: The arrays we are merging are [1] and [].
+The result of the merge is [1].
+Example 3:
+
+Input: nums1 = [0], m = 0, nums2 = [1], n = 1
+Output: [1]
+Explanation: The arrays we are merging are [] and [1].
+The result of the merge is [1].
+Note that because m = 0, there are no elements in nums1. The 0 is only there to ensure the merge result can fit in nums1.
+ 
+
+Constraints:
+
+nums1.length == m + n
+nums2.length == n
+0 <= m, n <= 200
+1 <= m + n <= 200
+-109 <= nums1[i], nums2[j] <= 109
+    
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 void merge(int* nums1, int nums1Size, int m, int* nums2, int nums2Size, int n){
-    int i=0,j=0,k=0;
-   int arr[m+n];
-    while(i<m && j<n){
-if(nums1[i]<nums2[j]){arr[k++]=nums1[i++];}
-else{arr[k++]=nums2[j++];}
+int i1=m-1,i2=n-1,i=nums1Size-1;
+    while(i1>=0 && i2>=0){
+        if(nums1[i1]>nums2[i2]){
+nums1[i--]=nums1[i1--];}
+        else
+        {
+nums1[i--]=nums2[i2--];}
     }
- for(;i<m;i++){
-        arr[k++]=nums1[i];
+if(i2>=0){
+while(i2>=0){
+nums1[i--]=nums2[i2--];
     }
-     for(;j<n;j++){
-        arr[k++]=nums2[j];
-    }
-     for(i=0;i<m+n;i++){
-        nums1[i]=arr[i];
-    }
+} 
 }
  int nums1[100],nums2[100];
 void main()
@@ -39,8 +77,9 @@ void main()
     for(int i=0;i<num1size;i++)
     {
         printf("%d",nums1[i]);
-    }
-
-    
+    } 
     
 }
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
