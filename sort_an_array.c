9d42b9ed56b5381a2fd4450e34117a2c05b3271d
Popuri Keerthika <79918843+keerthikapopuri@users.noreply.github.com>
2023-01-08 10:09:49 +0530

    Update sort_an_array.c

--- sort_an_array.c
+++ sort_an_array.c
@@ -23,53 +23,75 @@ Constraints:
 -5 * 104 <= nums[i] <= 5 * 104
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-#include <stdio.h>
-int nums[100];
-void merge(int* nums,int l,int mid,int h){
-    int b[h-l+1];
-    int i=l;
-    int j=mid+1;
-    int k=0;
-    while(i<=mid && j<=h){
-        b[k++]=nums[i]<nums[j]?nums[i++]:nums[j++];
-        
+#include <iostream>
+#include<vector>
+using namespace std;
+void merge(vector<int>&nums, int l, int mid, int r){
+    int n1 = mid -l+1;
+    int n2 = r - mid;
+    int a[n1];
+    int b[n2];
+    for(int i=0;i<n1;i++){
+        a[i] = nums[l+i];
     }
-    while(i<=mid){b[k++]=nums[i++];}
-    while(j<=h){b[k++]=nums[j++];}
-    k=0;
-    for(int i=l;i<=h;i++){
-        nums[i]=b[k++];
+    for(int j = 0;j<n2;j++){
+        b[j] = nums[mid+1+j];
     }
-}
-
-void mergesort(int* nums,int l,int h){
-    if(l<h){
-        int mid=(l+h)/2;
-        mergesort(nums,l,mid);
-        mergesort(nums,mid+1,h);
-        merge(nums,l,mid,h);
+  
+    int i = 0;
+    int j = 0;
+    int k = l;
+    while(i<n1 && j<n2){
+        if(a[i] < b[j]){
+            nums[k] = a[i];
+            i++;
+            k++;
+        }
+        else{
+            nums[k] = b[j];
+            j++;
+            k++;
+        }
     }
-}
-void sortarray(int* nums,int n,int* returnsize)
-{
-    *returnsize=n;
     
-    mergesort(nums,0,n-1);
-    for(int i=0;i<n;i++){
-        printf("%d",*(nums+i));
+    while(i<n1){
+        nums[k] = a[i];
+        i++;
+        k++;
+    }
+    while(j<n2){
+        nums[k] = b[j];
+        j++;
+        k++;
     }
 }
-int main()
-{
-    int n,returnsize;
-    printf("enter size");
-    scanf("%d",&n);
-    for(int i=0;i<n;i++)
-    {
-        scanf("%d",&nums[i]);
+   void mergeSort(vector<int>&nums,int l, int r){
+    if(l<r){
+        int mid = (l+r)/2;
+        mergeSort(nums,l,mid);
+        mergeSort(nums, mid+1, r);
+        
+        merge(nums , l, mid, r);
     }
-    sortarray(nums,n,&returnsize);
-
-    return 0;
+}
+    vector<int> sortArray(vector<int>& nums) {
+   int l = 0;
+   int r = nums.size() - 1;
+    mergeSort( nums, l, r);
+    return nums;
+}
+int main(){
+    vector<int> nums;
+    int n,a;
+    cout<<"enter size";
+    cin>>n;
+    for(int i=0;i<n;i++){
+        cin>>a;
+        nums.push_back(a);
+    }
+      sortArray(nums);
+      for(int i=0;i<n;i++){
+          printf("%d",nums[i]);
+      }
+    
 }
